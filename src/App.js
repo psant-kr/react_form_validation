0a68ae36133ccc7e069ddf4e36537c7505d4284e
prashant kumar <prashantdoa1996@gmail.com>
2023-06-14 00:35:40 +0530

    form validation done using react-hook-form but no css added add later

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { useForm } from "react-hook-form"
 import './App.css';
 
+
 function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const [userInfo, setUserInfo] = useState()
+
+  const onSubmit = (data) => {
+    setUserInfo(data)
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Registration form</h1>
+        <div>
+          <label>Username</label>
+          <input type='text' name='userName' placeholder='Username' {...register('userName', { required: "Username is required " })} />
+        </div>
+        <p>{errors.userName?.message}</p>
+        <div>
+          <label>Email</label>
+          <input type='email' name='email' placeholder='Email' {...register('email', {
+            required: " Email is required ", pattern: {
+              value: /^\S+@\S+$/i,
+              message: "This is not a valid email",
+            },
+          })} />
+        </div>
+        <p>{errors.email?.message}</p>
+        <div>
+          <label>Password</label>
+          <input type='password' name='password' placeholder='Password' {...register('password', {
+            required: "Password is required ", minLength: {
+              value: 6,
+              message: "Password must be more than 6 characters",
+            },
+            maxLength: {
+              value: 10,
+              message: "Password cannot exceed more than 10 characters",
+            },
+          })} />
+        </div>
+        <p>{errors.password?.message}</p>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
